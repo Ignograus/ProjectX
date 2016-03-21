767bd24e91f241cbc9aed43974dfebf08f28725c
Ignograus <Ignograus@live.de>
2016-03-21 23:09:08 +0100

    Update Main.java

--- src/com/projectx/Main.java
+++ src/com/projectx/Main.java
@@ -3,6 +3,6 @@ package com.projectx;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Bestes Programm");
+        System.out.println("Aenderung");
     }
 }

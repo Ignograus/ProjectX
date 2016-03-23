bb8503fe0d9341555fb9f108df48118b054a78c7
Ignograus <Ignograus@live.de>
2016-03-23 11:01:13 +0100

    More Stuff

---
+++ src/graphics/Shader.java
@@ -0,0 +1,66 @@
+package graphics;
+
+import math.Matrix4f;
+import math.Vector3f;
+import utils.ShaderUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.lwjgl.opengl.GL20.*;
+
+/**
+ * Created by Ignograus on 23.03.2016.
+ */
+public class Shader {
+    public static final int VERTEX_ATTRIB = 0;
+    public static final int TCOORD_ATTRIB = 1;
+
+    private final int ID;
+    private Map<String, Integer> locationCache = new HashMap<String, Integer>();
+
+    public Shader(String vertex, String fragment) {
+        ID = ShaderUtils.load(vertex, fragment);
+    }
+
+    public int getUniform(String name) {
+        if (locationCache.containsKey(name))
+            return locationCache.get(name);
+
+        int result = glGetUniformLocation(ID, name);
+        if( result == -1)
+            System.err.println("Could not find uniform variable: " + name);
+        else
+            locationCache.put(name, result);
+        return result;
+    }
+
+    public void setUniformli(String name, int value) {
+        glUniform1i(getUniform(name), value);
+    }
+
+    public void setUniformlf(String name, float value) {
+        glUniform1f(getUniform(name), value);
+    }
+
+    public void setUniform2f(String name, float x, float y) {
+        glUniform2f(getUniform(name), x, y);
+    }
+
+    public void setUniform3f(String name, Vector3f vector) {
+        glUniform3f(getUniform(name), vector.x, vector.y, vector.z);
+    }
+
+    public void setUniformMat4f(String name, Matrix4f matrix) {
+        glUniformMatrix4fv(getUniform(name), false, matrix.toFloatBuffer());
+    }
+
+
+    public void enable() {
+        glUseProgram(ID);
+    }
+
+    public void disable() {
+
+    }
+}
